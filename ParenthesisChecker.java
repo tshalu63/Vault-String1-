b6b54c837c81fdadcda70a2583290b9765539356
Shalu Tiwari <shalutiwari842@gmail.com>
2026-04-06 21:46:03 +0530

    Create  ParenthesisChecker.java

---
+++ ParenthesisChecker.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class ParenthesisChecker {
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) return false;
+
+                char top = stack.pop();
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValid("()[]{}")); // true
+    }
+}
